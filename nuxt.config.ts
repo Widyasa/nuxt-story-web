427245e57881d1e6ecd3f47e0cbe627cea7c916f
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-05-15 16:57:13 +0800

    change nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['@/assets/main.css', 'bootstrap/dist/css/bootstrap.min.css']
+  css: ['@/assets/main.css', 'bootstrap/dist/css/bootstrap.min.css'],
+  modules: [
+    // ...
+    '@pinia/nuxt',
+  ],
 })
