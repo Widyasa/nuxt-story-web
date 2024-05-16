732efc9784b864fd67251a321874dd76b5dbf353
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2024-05-16 11:54:20 +0800

    search data

--- stores/story.ts
+++ stores/story.ts
@@ -5,17 +5,26 @@ import {apiUrl} from "~/helpers/globalVariable";
 export const storyStore = defineStore('storyStore', {
     state : () => ({
         storyList : []  ,
-        pages: ref(1)
+        pages: ref(1),
+        keyword: ref('')
     }),
     actions : {
         async getStory() {
-            const {data} = await axios.get(apiUrl + `stories?keyword&author&page=${this.pages}`)
+            const {data} = await axios.get(apiUrl + `stories?keyword=${this.keyword}&author&page=${this.pages}`)
             if (this.pages > 1) {
 
                 data.data.forEach(item => this.storyList.push(item))
             } else {
                 this.storyList = data.data
             }
+        },
+        async searchStory() {
+            const {data} = await axios.get(apiUrl + `stories?keyword=${this.keyword}&author&page`)
+            this.storyList = data.data
+            if (this.keyword === '') {
+                await this.getStory()
+            }
+
         }
     }
 })
